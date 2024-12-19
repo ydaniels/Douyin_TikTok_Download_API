c3d3cf0e41bcb97191251ca07b116953d4ac28a7
ydaniels <yomid4all@gmail.com>
2024-12-19 03:06:33 +0100

    force h264 first

--- crawlers/hybrid/hybrid_crawler.py
+++ crawlers/hybrid/hybrid_crawler.py
@@ -165,12 +165,13 @@ class HybridCrawler:
             if url_type == 'video':
                 # 将信息储存在字典中/Store information in a dictionary
                 # wm_video = data['video']['downloadAddr']
-                if 'download_addr' in data['video']:
+                if 'play_addr_h264' in data['video']:
+                    wm_video = data['video']['play_addr_h264']['url_list'][0]
+                elif 'download_addr' in data['video']:
                     wm_video = data['video']['download_addr']['url_list'][0]
                 elif 'downloadAddr' in data['video']:
                     wm_video = data['video']['downloadAddr']['url_list'][0]
-                elif 'play_addr_h264' in data['video']:
-                    wm_video = data['video']['play_addr_h264']['url_list'][0]
+
                 elif 'play_addr' in data['video']:
                     wm_video = data['video']['play_addr']['url_list'][0]
                 elif 'playAddr' in data['video']:
